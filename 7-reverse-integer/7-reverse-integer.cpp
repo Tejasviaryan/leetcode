baa280f4321a33a88e8225a1a11259250bc91d91
Tejasviaryan <98518420+Tejasviaryan@users.noreply.github.com>
2022-06-11 02:50:55 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.97%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -3,9 +3,10 @@ public:
     int reverse(int x) {
         int output = 0;
         while(x != 0){
-            if(output > INT_MAX/10 || output < INT_MIN/10) //need to check this first (if input would overflow in next line) and if the value calculated will overflow
+             if(output > INT_MAX/10 || output < INT_MIN/10) 
                 return 0;
-            output = output*10 + x%10;
+           output = output*10 + x%10;
+             
             x /= 10;
         }
         return output;
